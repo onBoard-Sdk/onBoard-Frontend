50bb5d0b51c06b217ce7a097e43ff7d78576f28a
kimjh11130 <kimjh11130@naver.com>
2024-04-29 22:26:15 +0900

    docs: Input story 추가

--- apps/storybook/stories/Input/Input.stories.ts
+++ apps/storybook/stories/Input/Input.stories.ts
@@ -3,10 +3,17 @@ import { Meta } from "@storybook/react";
 
 export default {
   title: "Components/Input",
-  component: Input
+  component: Input,
+  tags: ["autodocs"],
 } as Meta<typeof Input>;
 
-
 export const InputStory = {
-  args: {},
+  args: {
+    label: "라벨",
+    width: "200px",
+    placeholder: "placeholder",
+    helpMessage: "도움말",
+    isInvalid: false,
+    disabled: false,
+  },
 };
