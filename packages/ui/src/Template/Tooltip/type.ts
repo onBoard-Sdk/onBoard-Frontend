729b37f1ccc59e37ff7a6e72bedba97c46c6068c
서유정 <seoyujeong1234@gmail.com>
2024-05-07 14:29:07 +0900

    fix: 타입 수정

--- packages/ui/src/Template/Tooltip/type.ts
+++ packages/ui/src/Template/Tooltip/type.ts
@@ -1,11 +1,9 @@
-import React from "react";
-
 export type TooltipProps = {
   emoji?: string;
   title: string;
   explanation: string;
   image?: string;
   count?: string;
-  onPrevStep?: React.MouseEventHandler<HTMLButtonElement>;
-  onNextStep?: React.MouseEventHandler<HTMLButtonElement>;
+  onPrevStep?: () => void;
+  onNextStep?: () => void;
 };
