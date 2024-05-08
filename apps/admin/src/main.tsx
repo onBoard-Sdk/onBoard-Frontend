e22c6605d71e3f5972eedb436935348c1c70d316
kimjh11130 <kimjh11130@naver.com>
2024-05-08 14:40:38 +0900

    fix: queryclient설정 삭제

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -3,13 +3,7 @@ import App from "./App.tsx";
 import "./style/globalStyle.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5000,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
