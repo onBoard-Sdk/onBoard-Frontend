62fc42b695a0564e66fac210e709e98595ee07ea
kimjh11130 <kimjh11130@naver.com>
2024-05-14 09:22:40 +0900

    feat: header 퍼블리싱

---
+++ apps/admin/src/components/header/index.tsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+import { logoImage } from "@/assets";
+import { Button } from "@onboard/ui";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  if (window.location.pathname === "/" || window.location.pathname === "/signup") {
+    return null;
+  }
+  return (
+    <StyledWrapper>
+      <Link to="/service">
+        <StlyedLogo src={logoImage} alt="Logo" />
+      </Link>
+      <Button buttonColor="gray">로그아웃</Button>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  box-shadow:
+    rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,
+    rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+  z-index: 100;
+`;
+
+const StlyedLogo = styled.img`
+  width: 160px;
+`;
