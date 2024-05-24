99474fa93bae82566fcaaee81d46a96af64f9cdb
김상구 <spair0039@gmail.com>
2024-05-24 11:46:16 +0900

    feat: 에디터 페이지 라우팅

--- apps/admin/src/router.tsx
+++ apps/admin/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "@/pages/login";
 import Signup from "@/pages/signup";
+import Editor from "@/pages/editor";
 
 export function Router() {
   return (
@@ -8,6 +9,7 @@ export function Router() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/editor/:guideId" element={<Editor />} />
       </Routes>
     </BrowserRouter>
   );
